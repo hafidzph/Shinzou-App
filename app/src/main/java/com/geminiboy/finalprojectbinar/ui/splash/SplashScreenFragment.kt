973b07c474a799dfbd92483ce8d4c66148a96ee9
Hafidz Putra Herlyansyah <apiiettrecomen1@gmail.com>
2023-06-06 08:27:39 +0700

    update package and adding Resource class.

--- app/src/main/java/com/geminiboy/finalprojectbinar/ui/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/geminiboy/finalprojectbinar/ui/splash/SplashScreenFragment.kt
@@ -1,18 +1,20 @@
-package com.geminiboy.finalprojectbinar.view.LoginRegister
+package com.geminiboy.finalprojectbinar.ui.splash
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.geminiboy.finalprojectbinar.R
 
-class LoginFragment : Fragment() {
+@SuppressLint("CustomSplashScreen")
+class SplashScreenFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
     }
 }
