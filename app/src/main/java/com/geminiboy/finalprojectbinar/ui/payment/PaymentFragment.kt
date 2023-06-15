b9f5548fb671b1620cf2b571b233fba7fcb1bf7c
rikiabdilah <rikiabdillah2002@gmail.com>
2023-06-15 21:38:34 +0700

    ui payment, ui paymentSucces - riki

---
+++ app/src/main/java/com/geminiboy/finalprojectbinar/ui/payment/PaymentFragment.kt
@@ -0,0 +1,25 @@
+package com.geminiboy.finalprojectbinar.ui.payment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.geminiboy.finalprojectbinar.R
+
+class PaymentFragment : Fragment() {
+
+
+    private lateinit var viewModel: PaymentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_payment, container, false)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/geminiboy/finalprojectbinar/ui/payment/PaymentViewModel.kt
@@ -0,0 +1,7 @@
+package com.geminiboy.finalprojectbinar.ui.payment
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
